4043ef80f43b3fa301df804cdc4b2c01955b9960
ogohogo <78665139+ogohogo@users.noreply.github.com>
2021-07-21 11:41:06 +0200

    make page more light

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,169 +4,175 @@ var video;
 var loaded;
 
 YT.updateManager = {
-    updateViews: function (a) {
+    updateViews: (a) => {
         document.querySelector(".views").innerHTML = a;
     },
-    updateLikes: function (a) {
+    updateLikes: (a) => {
         document.querySelector(".likes").innerHTML = a;
     },
-    updateDislikes: function (a) {
+    updateDislikes: (a) => {
         document.querySelector(".dislikes").innerHTML = a;
     },
-    updateComments: function (a) {
+    updateComments: (a) => {
         document.querySelector(".comments").innerHTML = a;
     },
-    updateTitle: function (a) {
+    updateTitle: (a) => {
         document.querySelectorAll(".title").forEach(t => {
             t.innerHTML = a;
         })
-    },
-    updateDescription: function (a) {
-        document.querySelector(".description").innerHTML = a;
-    }
-}
 
-YT.shareButtonsManager = {
-    updateTwitter: function(a) {
+        document.querySelector(".videoDetails").classList.remove("hidden")
+    },
+    updateDescription: (a) => {
+        document.querySelector(".description").innerHTML = a;
+    },
+
+    updateYear: (a) => {
+        document.querySelector(".year").innerHTML = a;
+    },
+
+    updateURL: (a) => {
+        if (location.href.endsWith("/")) history.pushState(null, null, `#!/${a}`);
+        else history.pushState(null, null, `/#!/${a}`);
+    },
+
+    updateTwitterURL: (a) => {
         document.querySelector(".shareTwitter").href = a
     }
 }
 
 YT.systemManager = {
-    getEstimatedViewCount: function (a, b, c) {
-        var channelId = a;
+    getEstimatedViewCount: (a, b, c) => {
+        var videoId = a;
         var views = parseInt(b);
         var likes = parseInt(c);
 
-        var localLikeCount = parseInt(localStorage.getItem('likeCount-' + channelId))
-        var localViewCount = parseInt(localStorage.getItem('viewCount-' + channelId))
+        var localLikeCount = parseInt(localStorage.getItem('likeCount-' + videoId))
+        var localViewCount = parseInt(localStorage.getItem('viewCount-' + videoId))
         var ratio = views / likes;
         if (!localViewCount) {
-            localStorage.setItem('likeCount-' + channelId, parseInt(likes));
-            localStorage.setItem('viewCount-' + channelId, parseInt(views));
+            localStorage.setItem('likeCount-' + videoId, parseInt(likes));
+            localStorage.setItem('viewCount-' + videoId, parseInt(views));
         }
         if (localViewCount != views) {
-            localStorage.setItem('viewCount-' + channelId, parseInt(views));
-            localStorage.setItem('likeCount-' + channelId, parseInt(likes));
+            localStorage.setItem('viewCount-' + videoId, parseInt(views));
+            localStorage.setItem('likeCount-' + videoId, parseInt(likes));
             return views;
         }
         var estViewCount = Math.round(views + (likes - localLikeCount) * ratio);
         return estViewCount;
     },
 
-    getVideoInURL: function () {
-        return window.location.href.split("#/")[1]
+    getVideoInURL: () => {
+        return window.location.href.split("#!/")[1]
     },
 
     loaderFadeOut: function () {
-        $("#loader-wrapper").fadeOut();
+        document.querySelector("#loader-wrapper").classList.toggle('fade');
+        setTimeout(() => {
+            document.querySelector("#loader-wrapper").classList.add("hidden")
+        }, 500)
     },
 
     loaderFadeIn: function () {
-        $("#loader-wrapper").fadeIn(100);
+        document.querySelector("#loader-wrapper").classList.toggle('fade');
+        document.querySelector("#loader-wrapper").classList.remove("hidden")
     }
 
 }
 
 YT.dataManager = {
-    getData: () => {
-        if (video == undefined) return;
+    getData: async () => {
+        if (!video) return;
 
-        $.getJSON(`${APIUrl}/api/live-view-count/data/${video}`, function (data) {
-            YT.updateManager.updateTitle(data.name)
-            YT.updateManager.updateDescription(data.description)
+        const data = await fetch(`${APIUrl}/api/live-view-count/data/${video}`).then(res => res.json())
 
-            YT.shareButtonsManager.updateTwitter(`https://twitter.com/intent/tweet?url=&text=Watch%20${data.name}'s%20Live%20View%2C%20Like%2C%20Dislike%20and%20Comment%20count%20on%20https%3A%2F%2Fwww.youtubelikecounter.com%2F%23%2F${video}%20!`)
-        })
+        YT.updateManager.updateTitle(data.name)
+        YT.updateManager.updateDescription(data.description)
+
+        YT.updateManager.updateTwitterURL(`https://twitter.com/intent/tweet?url=&text=Watch%20${data.name}'s%20Live%20View%2C%20Like%2C%20Dislike%20and%20Comment%20count%20on%20https%3A%2F%2Fwww.youtubelikecounter.com%2F%23%2F${video}%20!`)
     },
-    getStats: () => {
-        $.getJSON(`${APIUrl}/api/live-view-count/stats/${video}`, function (data) {
-            if (data.viewerCount != null) YT.updateManager.updateViews(data.viewerCount)
-            else YT.updateManager.updateViews(YT.systemManager.getEstimatedViewCount(video, data.followerCount, data.bottomOdos[0]))
+    getStats: async () => {
+        const data = await fetch(`${APIUrl}/api/live-view-count/stats/${video}`).then(res => res.json())
 
-            YT.updateManager.updateLikes(data.bottomOdos[0])
-            YT.updateManager.updateDislikes(data.bottomOdos[1])
-            YT.updateManager.updateComments(data.bottomOdos[2])   
-        })
+        YT.updateManager.updateViews(data.viewerCount && data.viewerCount || YT.systemManager.getEstimatedViewCount(video, data.followerCount, data.bottomOdos[0]))
+        YT.updateManager.updateLikes(data.bottomOdos[0])
+        YT.updateManager.updateDislikes(data.bottomOdos[1])
+        YT.updateManager.updateComments(data.bottomOdos[2])
     }
 }
 
 YT.searchManager = {
-    search: function (a) {
-        if (a.includes("youtube.com/watch?v=")) {
-            video = a.split("watch?v=")[1]
+    search: async (a) => {
+
+        if (a.includes("youtube.com/watch?v=") || a.includes("youtu.be")) {
+            video = a.split("/")[3].replace("watch?v=", "").split("&")[0]
 
             YT.systemManager.loaderFadeIn();
             YT.dataManager.getData();
             YT.dataManager.getStats();
+            YT.updateManager.updateURL(video)
 
-            if (location.href.endsWith("/")) history.pushState(null, null, `#/${video}`);
-            else history.pushState(null, null, `/#/${video}`);
-
-            setTimeout(function () {
-                document.querySelector(".afterVideoLoaded").classList.remove("hidden")
-                YT.systemManager.loaderFadeOut();
-            }, 250)
-
-        } else {
-            $.getJSON(`${APIUrl}/api/live-view-count/search/${a}`, function (data) {
-                if (data.userData.length < 1) alert("No Results Found!")
-                else {
-                    video = data.userData[0].id
-
-                    YT.systemManager.loaderFadeIn();
-                    YT.dataManager.getData();
-                    YT.dataManager.getStats();
-
-                    if (location.href.endsWith("/")) history.pushState(null, null, `#/${video}`);
-                    else history.pushState(null, null, `/#/${video}`);
-
-                    setTimeout(function () {
-                        document.querySelector(".afterVideoLoaded").classList.remove("hidden")
-                        YT.systemManager.loaderFadeOut();
-                    }, 250)
-                }
-            })
+            document.querySelector(".afterVideoLoaded").classList.remove("hidden")
+            return YT.systemManager.loaderFadeOut();
         }
+
+        const data = await fetch(`${APIUrl}/api/live-view-count/search/${a}`).then(res => res.json());
+        if (data.userData.length < 1) return alert("No Results Found!");
+
+        video = data.userData[0].id
+
+        YT.systemManager.loaderFadeIn();
+        YT.dataManager.getData();
+        YT.dataManager.getStats();
+        YT.updateManager.updateURL(video)
+
+        document.querySelector(".afterVideoLoaded").classList.remove("hidden")
+        YT.systemManager.loaderFadeOut();
     }
 }
 
-setTimeout(function () {
+setTimeout(() => {
 
-    
-    if (window.location.href.includes("#/")) {
+    YT.updateManager.updateYear(new Date().getFullYear())
+
+    if (window.location.href.includes("#!/")) {
         video = YT.systemManager.getVideoInURL()
         YT.dataManager.getData();
         YT.dataManager.getStats();
-        setTimeout(function () {
-            document.querySelector(".afterVideoLoaded").classList.remove("hidden")
-            YT.systemManager.loaderFadeOut();
-        }, 250)
+
+        document.querySelector(".afterVideoLoaded").classList.remove("hidden")
+        return YT.systemManager.loaderFadeOut();
     } else {
-        YT.systemManager.loaderFadeOut();
+        return YT.systemManager.loaderFadeOut();
     }
+
 })
 
-setInterval(function () {
-    if (video == undefined) return;
+setInterval(() => {
+    if (!video) return;
 
     YT.dataManager.getStats();
-}, 3000)
+}, 3 * 1000)
 
-document.querySelector(".searchInput").addEventListener("keyup", function(event) {
-    if (event.keyCode === 13) {
-        let val = document.querySelector(".searchInput").value
-        if (val != 0) YT.searchManager.search(val)
-    }
+document.querySelector(".searchInput").addEventListener("keyup", (event) => {
+    if (event.keyCode != 13) return;
+
+    let val = document.querySelector(".searchInput").value
+    if (val < 1) return alert("Search Query can't be empty!")
+    return YT.searchManager.search(val)
 });
 
-document.querySelector(".searchButton").onclick = function() {
-    let val = document.querySelector(".searchInput").value
-    if (val != 0) YT.searchManager.search(val) 
+document.querySelector(".searchButton").onclick = () => {
+
+    let val = document.querySelector(".searchInput").value;
+    if (val < 1) return alert("Search Query can't be empty!");
+    return YT.searchManager.search(val);
+
 }
 
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
 
-  gtag('config', 'UA-119417406-9');
+gtag('config', 'UA-119417406-9');
